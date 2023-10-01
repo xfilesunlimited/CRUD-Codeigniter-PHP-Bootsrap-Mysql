6d2174fc47d77fd983f18bd7551ca047d5c63c14
xfilesunlimited <overhandknot86@gmail.com>
2023-10-01 12:49:13 +0800

    First Commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+
+ 
+// CRUD Routes
+$routes->get('student', 'Student::index');
+$routes->post('student/store', 'Student::store');
+$routes->get('student/edit/(:num)', 'Student::edit/$1');
+$routes->get('student/delete/(:num)', 'Student::delete/$1');
+$routes->post('student/update', 'Student::update');
